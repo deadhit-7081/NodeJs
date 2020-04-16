699d15e6d251fa000134b786efde20558cf78bff
Jai <jaikumarsingh1908@gmail.com>
2020-04-16 19:21:38 +0530

    Simple Example

---
+++ index.js
@@ -0,0 +1,23 @@
+var rect= {
+    perimeter:(x,y) => (2*(x+y)),
+    area:(x,y) => (x*y)
+}
+
+function solveRect(l,b)
+{
+    console.log("Solving for rect with l= "+l +" and b= "+b);
+
+    if(l<=0 || b<=0)
+    {
+        console.log("Rect dim should be >0")
+    }
+    else{
+        console.log("area :"+rect.area(l,b));
+        console.log("peri :"+rect.perimeter(l,b));
+    }
+}
+
+solveRect(2,4)
+solveRect(0,0)
+solveRect(-12,5)
+solveRect(5,5)
